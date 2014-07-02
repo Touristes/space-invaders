108e9e2d9624227727a223d43163c6b8982a78f7
DemisBand <Tom@air-de-thomas.home>
2014-07-02 21:57:29 +0200

    Test Menu + Ajout SDL TTF (aff texte)

--- src/draw.c
+++ src/draw.c
@@ -1,6 +1,6 @@
 #include "../SDL2/SDL.h"
 #include <stdio.h>
-
+#include "../SDL2/SDL_ttf.h"
 SDL_Renderer* drawGame(SDL_Renderer* pRenderer)
 {
   // Remplis le renderer de noir, efface l'écran et l'affiche.
@@ -15,10 +15,11 @@ SDL_Renderer* drawMenu(SDL_Renderer* pRenderer, int selectedMenu)
 {
   // Remplis le renderer de noir, efface l'écran et l'affiche.
   //SDL_RenderPresent() remplace SDL_Flip de la SDL 1.2
-  SDL_SetRenderDrawColor(pRenderer, 255, 255, 255, 255);
+        //Le titre
+  SDL_SetRenderDrawColor(pRenderer, 0, 0, 0, 255);
   SDL_RenderClear(pRenderer);
   SDL_SetRenderDrawColor(pRenderer, 80, 80, 150, 255);
-  if (selectedMenu == 1)
+    if (selectedMenu == 1)
     SDL_SetRenderDrawColor(pRenderer, 180, 180, 250, 255);
   else
     SDL_SetRenderDrawColor(pRenderer, 80, 80, 150, 255);
