ffe5c5124d8851403f43a88800699bf7bff79148
FloGro <grogne_f@etna-alternance.net>
2014-07-16 17:45:46 +0200

    String.c

--- src/writeScore.c
+++ src/writeScore.c
@@ -21,24 +21,3 @@ int	writeScore(char *name, int points) {
  fclose(file);;
  return(0);
 }
-
-void	intToString(int nbr, char *string) {
-  int i;
-
-  for (i = 8; i >= 0; i--) {
-    string[i] = (nbr % 10) + '0';
-    nbr = nbr / 10;
-  }
-  string[9] = '\0';
-}
-
-int	stringToInt(char *string) {
-  int nbr;
-  int i;
-
-  nbr = 0;
-  for (i = 8; i > 0 ;i--) {
-    nbr = nbr + ((int)string[i] * i);
-  }
-  return (nbr);
-}
